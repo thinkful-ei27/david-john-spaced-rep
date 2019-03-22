44d35df03aab2a1fae3f5c22446ddc34b0dca295
John Young <john.young@spreadstreet.io>
2019-03-22 11:49:46 -0400

    Fixing merge issues that WERE NOT OUR FAULT

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -5,21 +5,30 @@ import Correct from "./Correct";
 function Messages(props) {
   const { textArea } = props;
   const messages = textArea.map(msg => {
-    const { type, userName, value } = msg;
+    const { type, userName, value, score } = msg;
+    if (type === "server") {
+      return (
+        <div
+          className="bg-blue-lightest border-t border-b border-blue text-blue-dark px-4 py-3 mb-8"
+          role="alert"
+        >
+          <p className="font-bold text-center">{value}</p>
+        </div>
+      );
+    }
     return (
       <>
         <li className="mb-4">
           <span className="ml-2 mr-4 font-bold w-1/5">{userName}</span>
           <span className="w-4/5">{value}</span>
         </li>
-        {type === "answer" ? <Correct userName={userName} /> : ""}
+        {type === "answer" ? <Correct userName={userName} score={score} /> : ""}
       </>
     );
   });
   return (
-    <ul className="mt-8 list-reset w-full max-w-lg h-64 max-h-full flex flex-col break-words overflow-auto">
+    <ul className="mt-8 list-reset w-full max-w-lg flex flex-col break-words">
       {messages}
-    <div ref></div>
     </ul>
   );
 }
